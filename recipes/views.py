327fb559a2ebcff92c2f4a3fc313c10fcc64d105
Danilo Moreira <danilolex5@gmail.com>
2026-09-11 17:50:06 -0300

    Update 11

--- recipes/views.py
+++ recipes/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from utils.recipes.factory import make_recipe
 from .models import Recipe
+from django.http import Http404
 # Create your views here.
 
 def home(request):
@@ -11,7 +12,15 @@ def home(request):
 
 def category(request, category_id):
     recipes = Recipe.objects.filter(category__id=category_id, is_published=True).order_by('-id')
-    return render(request, 'recipes/pages/category.html', context={'recipes': recipes,})
+
+    if not recipes:
+        raise Http404('Not found')
+
+
+    return render(request, 'recipes/pages/category.html', context={
+        'recipes': recipes,
+        'title':f'{recipes.first().category.name} - Category |'
+        })
 
 
 def recipe(request, id):
